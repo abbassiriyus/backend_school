71834834005b8a26fc3341fa92f22cf89f1de630
simple123w <122342661+Asliddin18@users.noreply.github.com>
2023-04-17 17:42:26 +0500

    12
    12

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 const { Pool } = require("pg")
 
-const pool = new Pool({
+/* const pool = new Pool({
     user: 'postgres',
     password: "U_A1118Tiny",
     host: "localhost",
     port: 5432,
     database: "Test"
-})
+}) */
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     password: "abbas123",
-//     host: "localhost",
-//     port: 5432,
-//     database: "Kindergarten_DB"
-// })
+const pool = new Pool({
+    user: 'postgres',
+    password: "lZzucb90bhHJmPWh3o8d",
+    host: "containers-us-west-177.railway.app",
+    port: 6696,
+    database: "railway"
+})
 
 pool.connect((err) => {
     if (!err) {
